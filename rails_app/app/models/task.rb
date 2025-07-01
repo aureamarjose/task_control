8ae9e1424ecb2cdd73ae5b7cf69b8aed43e4aba6
aureamar <aureamar.jose@gmail.com>
2025-07-01 21:52:56 +0000

    Ajustes na view task

--- rails_app/app/models/task.rb
+++ rails_app/app/models/task.rb
@@ -2,5 +2,5 @@
 
 class Task < ApplicationRecord
   belongs_to :collaborator
-  enum :task_status, { pending: 0, in_progress: 1, completed: 2 }
+  enum :task_status, { not_started: 0, in_progress: 1, completed: 2, canceled: 3 }
 end
