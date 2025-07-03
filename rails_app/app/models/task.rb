79e4dbfab929c4a62eec6c605eac7a7736fed8f1
aureamar <aureamar.jose@gmail.com>
2025-07-03 21:54:25 +0000

    Tradução Enums

--- rails_app/app/models/task.rb
+++ rails_app/app/models/task.rb
@@ -3,4 +3,9 @@
 class Task < ApplicationRecord
   belongs_to :collaborator
   enum :task_status, { not_started: 0, in_progress: 1, completed: 2, canceled: 3 }
+
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :start_date, presence: true
+  validates :collaborator_id, presence: true
+  validates :task_status, presence: true
 end
